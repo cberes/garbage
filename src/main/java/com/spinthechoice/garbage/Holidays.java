17c6cbf851aac88af5169971ba43d1d5435d0d12
Corey Beres <corey.beres@gmail.com>
2020-03-13 00:25:48 -0400

    make Holidays public

--- src/main/java/com/spinthechoice/garbage/Holidays.java
+++ src/main/java/com/spinthechoice/garbage/Holidays.java
@@ -11,14 +11,14 @@ import static java.util.stream.IntStream.rangeClosed;
 /**
  * Calculates actual dates of holidays given collections of holiday definitions.
  */
-class Holidays {
+public class Holidays {
     private final Set<Holiday> holidays;
 
     /**
      * Creates a new instance using the specified holidays.
      * @param holidays holidays
      */
-    Holidays(final Holiday... holidays) {
+    public Holidays(final Holiday... holidays) {
         this(new HashSet<>(asList(holidays)));
     }
 
@@ -26,7 +26,7 @@ class Holidays {
      * Creates a new instance using the specified holidays.
      * @param holidays holidays
      */
-    Holidays(final Collection<Holiday> holidays) {
+    public Holidays(final Collection<Holiday> holidays) {
         this.holidays = holidays == null ? emptySet() : new HashSet<>(holidays);
     }
 
@@ -35,7 +35,7 @@ class Holidays {
      * @param year year
      * @return dates of holidays
      */
-    Set<LocalDate> dates(final int year) {
+    public Set<LocalDate> dates(final int year) {
         // for day offsets to work for all days, this needs to be run for the next and previous years
         // in addition to the current year; then remove all days not within the given year
         return rangeClosed(year - 1, year + 1)
